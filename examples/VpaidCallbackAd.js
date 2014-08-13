f79f34b584ff7fc991918c6fbab8163375401be2
Ryan Thompson <ryanthompson@google.com>
2014-08-13 14:58:47 -0400

    updated callback ad

--- examples/VpaidCallbackAd.js
+++ examples/VpaidCallbackAd.js
@@ -1,7 +1,9 @@
-/*
+/**
  * @fileoverview A VPAID ad useful for testing functionality of the sdk.
  *
+ * @author ryanthompson@google.com (Ryan Thompson)
  */
+
 
 
 /**
@@ -16,7 +18,7 @@ var VpaidAd = function() {
   this.videoSlot_ = null;
   // An object containing all registered events.  These events are all
   // callbacks from the vpaid ad.
-  this.events_ = {};
+  this.eventCallbacks_ = {};
   // A list of attributes getable and setable.
   this.attributes_ = {
     'companions' : '',
@@ -39,92 +41,88 @@ var VpaidAd = function() {
  * Html to populate into the ad.  This provides all UI elements for the ad.
  */
 VpaidAd.HTML_TEMPLATE =
-  '<div style="background:blue">' +
-  '<div>Tracking events ' +
-  '<select id="eventSelect">' +
-    '<option>----</option>' +
-    '<option value="AdStarted">AdStarted</option>' +
-    '<option value="AdStopped">AdStopped</option>' +
-    '<option value="AdLoaded">AdLoaded</option>' +
-    '<option value="AdLinearChange">AdLinearChange</option>' +
-    '<option value="AdSizeChange">AdSizeChange</option>' +
-    '<option value="AdExpandedChange">AdExpandedChange</option>' +
-    '<option value="AdSkippableStateChange">AdSkippableStateChange</option>' +
-    '<option value="AdDurationChange">AdDurationChange</option>' +
-    '<option value="AdRemainingTimeChange">AdRemainingTimeChange</option>' +
-    '<option value="AdVolumeChange">AdVolumeChange</option>' +
-    '<option value="AdImpression">AdImpression</option>' +
-    '<option value="AdVideoStart">AdVideoStart</option>' +
-    '<option value="AdVideoFirstQuartile">AdVideoFirstQuartile</option>' +
-    '<option value="AdVideoMidpoint">AdVideoMidpoint</option>' +
-    '<option value="AdVideoThirdQuartile">AdVideoThirdQuartile</option>' +
-    '<option value="AdVideoComplete">AdVideoComplete</option>' +
-    '<option value="AdUserAcceptInvitation">AdUserAcceptInvitation</option>' +
-    '<option value="AdUserMinimize">AdUserMinimize</option>' +
-    '<option value="AdUserClose">AdUserClose</option>' +
-    '<option value="AdPaused">AdPaused</option>' +
-    '<option value="AdPlaying">AdPlaying</option>' +
-  '</select></div><br>' +
-  '<div>AdClickThru (make sure player handles works)' +
-    '<input type="text" id="clickThruUrl" value="http://fake.com"/>' +
-    '<input type="text" id="clickThruId" value="1"/>' +
-    '<input type="text" id="clickThruPlayerHandels" value="false"/>' +
-    '<input type="button" id="clickThruButton"/ value="AdClickThru">' +
-  '</div>' +
-  '<div>AdError' +
-    '<input type="text" id="adErrorMsg" value="ad error message"/>' +
-    '<input type="button" id="adErrorButton"/ value="AdError">' +
-  '</div>' +
-  '<div>AdLog' +
-    '<input type="text" id="adLogMsg" value="ad log message"/>' +
-    '<input type="button" id="adLogButton"/ value="AdLog">' +
-  '</div>' +
-  '<div>AdInteraction' +
-    '<input type="text" id="adInteractionId" value="1"/>' +
-    '<input type="button" id="adInteractionButton"/ value="AdInteraction">' +
-  '</div>' +
-  '<div> Last Action <textarea id="lastAction">None</textarea></div>' +
-  '<table>' +
-    '<tr>' +
-      '<td>Companions</td>' +
-      '<td>Desired Bitrate</td>' +
-      '<td>Duration</td>' +
-      '<td>Expanded</td>' +
-    '</tr>' +
-    '<tr>' +
-      '<td><input type="text" id="companions"/></td>' +
-      '<td><input type="text" id="desiredBitrate"/></td>' +
-      '<td><input type="text" id="duration"/></td>' +
-      '<td><input type="text" id="expanded"/></td>' +
-    '</tr>' +
-    '<tr>' +
-      '<td>Height</td>' +
-      '<td>Icons</td>' +
-      '<td>Linear</td>' +
-      '<td>Remaining Time</td>' +
-    '</tr>' +
-    '<tr>' +
-      '<td><input type="text" id="height"/></td>' +
-      '<td><input type="text" id="icons"/></td>' +
-      '<td><input type="text" id="linear"/></td>' +
-      '<td><input type="text" id="remainingTime"/></td>' +
-    '</tr>' +
-    '<tr>' +
-      '<td>Skippable State</td>' +
-      '<td>ViewMode</td>' +
-      '<td>Volume</td>' +
-      '<td>Width</td>' +
-    '</tr>' +
-    '<tr>' +
-      '<td><input type="text" id="skippableState"/></td>' +
-      '<td><input type="text" id="volume"/></td>' +
-      '<td><input type="text" id="viewMode"/></td>' +
-      '<td><input type="text" id="width"/></td>' +
-    '</tr>' +
-  '</table>' +
-  '<textarea rows="10" cols="50" id="logTextArea">' +
-  '</textarea>' +
-  '</div>';
+    '<div style="background:#f5f5f5; width:100%; height:100%">' +
+    '<div style="height: 100%;' +
+    '    display: inline-block; float:left;">' +
+    '<select id="eventSelect" size="10">' +
+    '  <option value="AdStarted">AdStarted</option>' +
+    '  <option value="AdStopped">AdStopped</option>' +
+    '  <option value="AdLoaded">AdLoaded</option>' +
+    '  <option value="AdLinearChange">AdLinearChange</option>' +
+    '  <option value="AdSizeChange">AdSizeChange</option>' +
+    '  <option value="AdExpandedChange">AdExpandedChange</option>' +
+    '  <option value="AdSkippableStateChange">AdSkippableStateChange</option>' +
+    '  <option value="AdDurationChange">AdDurationChange</option>' +
+    '  <option value="AdRemainingTimeChange">AdRemainingTimeChange</option>' +
+    '  <option value="AdVolumeChange">AdVolumeChange</option>' +
+    '  <option value="AdImpression">AdImpression</option>' +
+    '  <option value="AdVideoStart">AdVideoStart</option>' +
+    '  <option value="AdVideoFirstQuartile">AdVideoFirstQuartile</option>' +
+    '  <option value="AdVideoMidpoint">AdVideoMidpoint</option>' +
+    '  <option value="AdVideoThirdQuartile">AdVideoThirdQuartile</option>' +
+    '  <option value="AdVideoComplete">AdVideoComplete</option>' +
+    '  <option value="AdUserAcceptInvitation">AdUserAcceptInvitation</option>' +
+    '  <option value="AdUserMinimize">AdUserMinimize</option>' +
+    '  <option value="AdUserClose">AdUserClose</option>' +
+    '  <option value="AdPaused">AdPaused</option>' +
+    '  <option value="AdPlaying">AdPlaying</option>' +
+    '  <option value="AdClickThru">AdClickThru</option>' +
+    '  <option value="AdError">AdError</option>' +
+    '  <option value="AdLog">AdLog</option>' +
+    '  <option value="AdInteraction">AdInteraction</option>' +
+    '</select>' +
+    '</div>' +
+    '<div>' +
+    '<table>' +
+    '  <tr>' +
+    '    <td><b>companions</b><br><span id="companions">None</span></td>' +
+    '    <td><b>desired bitrate</b><br>' +
+    '       <span id="desiredBitrate">-1</span></td>' +
+    '    <td><b>duration</b><br><span id="duration">-1</span></td>' +
+    '  </tr>' +
+    '  <tr>' +
+    '    <td><b>expanded</b><br><span id="expanded">false</span></td>' +
+    '    <td><b>height</b><br><span id="height">-1</span></td>' +
+    '    <td><b>icons</b><br><span id="icons">None</span></td>' +
+    '  </tr>' +
+    '  <tr>' +
+    '    <td><b>linear</b><br><span id="linear">True</span></td>' +
+    '    <td><b>remaining time</b><br><span id="remainingTime">-1</span></td>' +
+    '    <td><b>skippable state</b><br>' +
+    '         <span id="skippableState">False</span></td>' +
+    '  </tr>' +
+    '  <tr>' +
+    '    <td><b>volume</b><br><span id="volume">1.0</span></td>' +
+    '    <td><b>view mode</b><br><span id="viewMode">normal</span></td>' +
+    '    <td><b>width</b><br><span id="width">5</span></td>' +
+    '  </tr>' +
+    '</table>' +
+    '<div>' +
+    '<hr>' +
+    '<div id="AdClickThruOptions" style="display:none;">' +
+    '  Click Through URL <input type="text" id="clickThruUrl"' +
+    '    value="http://example.com"/><br>' +
+    '  ID <input type="text" id="clickThruId" value="1"/><br>' +
+    '  Player Handles <input type="text" id="clickThruPlayerHandels"' +
+    '     value="false"/><br>' +
+    '</div>' +
+    '<div id="AdErrorOptions" style="display:none;">' +
+    '  AdError <input type="text" id="adErrorMsg" value="ad error message"/>' +
+    '</div>' +
+    '<div id="AdLogOptions" style="display:none;">' +
+    '  AdLog <input type="text" id="adLogMsg" value="ad log message"/>' +
+    '</div>' +
+    '<div id="AdInteractionOptions" style="display:none;">' +
+    '  AdInteraction <input type="text" id="adInteractionId" value="1"/>' +
+    '</div>' +
+    '</div>' +
+    '<h2><input type="button" id="triggerEvent" value="Trigger Event"/></h2>' +
+    '</div>' +
+    '<div style="position:fixed; bottom:10px">' +
+    '  Last event from player <input type="text" style="width:200px"' +
+    '     id="lastVpaidEvent" value=""/>' +
+    '</div>' +
+    '</div>';
 
 
 /**
@@ -159,7 +157,7 @@ VpaidAd.prototype.initAd = function(
   this.renderSlot_();
   this.addButtonListeners_();
   this.fillProperties_();
-  this.events_['AdLoaded']();
+  this.eventCallbacks_['AdLoaded']();
 };
 
 
@@ -182,29 +180,37 @@ VpaidAd.prototype.renderSlot_ = function() {
 
 /**
  * Adds all listeners to buttons.
- *
  * @private
  */
 VpaidAd.prototype.addButtonListeners_ = function() {
   var eventSelect = document.getElementById('eventSelect');
   eventSelect.addEventListener('change', this.eventSelected_.bind(this));
-  var clickThruButton = document.getElementById('clickThruButton');
-  clickThruButton.addEventListener(
-      'click',
-      this.adClickThruHandler_.bind(this));
-  var adErrorButton = document.getElementById('adErrorButton');
-  adErrorButton.addEventListener(
-      'click',
-      this.adErrorHandler_.bind(this));
-  var adLogButton = document.getElementById('adLogButton');
-  adLogButton.addEventListener(
-      'click',
-      this.adLogHandler_.bind(this));
-  var adInteractionButton = document.getElementById('adInteractionButton');
-  adInteractionButton.addEventListener(
-      'click',
-      this.adInteractionHandler_.bind(this));
+
+  var triggerEvent = document.getElementById('triggerEvent');
+  triggerEvent.addEventListener('click', this.triggerEvent_.bind(this));
 };
+
+
+/**
+ * Triggers an event.
+ * @private
+ */
+VpaidAd.prototype.triggerEvent_ = function() {
+  var eventSelect = document.getElementById('eventSelect');
+  var value = eventSelect.value;
+  if (value == 'AdClickThru') {
+    this.adClickThruHandler_();
+  } else if (value == 'AdError') {
+    this.adErrorHandler_();
+  } else if (value == 'AdLog') {
+    this.adLogHandler_();
+  } else if (value == 'AdInteraction') {
+    this.adInteractionHandler_();
+  } else if (value in this.eventCallbacks_) {
+    this.eventCallbacks_[value]();
+  }
+};
+
 
 /**
  * Returns the versions of vpaid ad supported.
@@ -221,8 +227,8 @@ VpaidAd.prototype.handshakeVersion = function(version) {
  */
 VpaidAd.prototype.startAd = function() {
   this.log('Starting ad');
-  if ('AdStart' in this.events_) {
-    this.events_['AdStarted']();
+  if ('AdStart' in this.eventCallbacks_) {
+    this.eventCallbacks_['AdStarted']();
   }
 };
 
@@ -232,10 +238,11 @@ VpaidAd.prototype.startAd = function() {
  */
 VpaidAd.prototype.stopAd = function() {
   this.log('Stopping ad');
-  if ('AdStop' in this.events_) {
-    this.events_['AdStopped']();
+  if ('AdStop' in this.eventCallbacks_) {
+    this.eventCallbacks_['AdStopped']();
   }
 };
+
 
 /**
  * @param {number} value The volume in percentage.
@@ -243,8 +250,8 @@ VpaidAd.prototype.stopAd = function() {
 VpaidAd.prototype.setAdVolume = function(value) {
   this.attributes_['volume'] = value;
   this.log('setAdVolume ' + value);
-  if ('AdVolumeChanged' in this.events_) {
-    this.events_['AdVolumeChanged']();
+  if ('AdVolumeChanged' in this.eventCallbacks_) {
+    this.eventCallbacks_['AdVolumeChanged']();
   }
 };
 
@@ -268,30 +275,33 @@ VpaidAd.prototype.resizeAd = function(width, height, viewMode) {
   this.attributes_['width'] = width;
   this.attributes_['height'] = height;
   this.attributes_['viewMode'] = viewMode;
-  if ('AdSizeChange' in this.events_) {
-    this.events_['AdSizeChange']();
+  if ('AdSizeChange' in this.eventCallbacks_) {
+    this.eventCallbacks_['AdSizeChange']();
   }
 };
+
 
 /**
  * Pauses the ad.
  */
 VpaidAd.prototype.pauseAd = function() {
   this.log('pauseAd');
-  if ('AdPaused' in this.events_) {
-    this.events_['AdPaused']();
+  if ('AdPaused' in this.eventCallbacks_) {
+    this.eventCallbacks_['AdPaused']();
   }
 };
+
 
 /**
  * Resumes the ad.
  */
 VpaidAd.prototype.resumeAd = function() {
   this.log('resumeAd');
-  if ('AdResumed' in this.events_) {
-    this.events_['AdResumed']();
+  if ('AdResumed' in this.eventCallbacks_) {
+    this.eventCallbacks_['AdResumed']();
   }
 };
+
 
 /**
  * Expands the ad.
@@ -299,10 +309,11 @@ VpaidAd.prototype.resumeAd = function() {
 VpaidAd.prototype.expandAd = function() {
   this.log('expandAd');
   this.attributes_['expanded'] = true;
-  if ('AdExpanded' in this.events_) {
-    this.events_['AdExpanded']();
+  if ('AdExpanded' in this.eventCallbacks_) {
+    this.eventCallbacks_['AdExpanded']();
   }
 };
+
 
 /**
  * Returns true if the ad is expanded.
@@ -342,8 +353,8 @@ VpaidAd.prototype.skipAd = function() {
   this.log('skipAd');
   var skippableState = this.attributes_['skippableState'];
   if (skippableState) {
-    if ('AdSkipped' in this.events_) {
-      this.events_['AdSkipped']();
+    if ('AdSkipped' in this.eventCallbacks_) {
+      this.eventCallbacks_['AdSkipped']();
     } else {
       this.log('Error: Invalid ad skip request.');
     }
@@ -360,7 +371,7 @@ VpaidAd.prototype.skipAd = function() {
 VpaidAd.prototype.subscribe = function(aCallback, eventName, aContext) {
   this.log('Subscribe ' + aCallback);
   var callBack = aCallback.bind(aContext);
-  this.events_[eventName] = callBack;
+  this.eventCallbacks_[eventName] = callBack;
 };
 
 
@@ -371,7 +382,7 @@ VpaidAd.prototype.subscribe = function(aCallback, eventName, aContext) {
  */
 VpaidAd.prototype.unsubscribe = function(eventName) {
   this.log('unsubscribe ' + eventName);
-  this.events_[eventName] = null;
+  this.eventCallbacks_[eventName] = null;
 };
 
 
@@ -422,6 +433,7 @@ VpaidAd.prototype.getAdIcons = function() {
   return this.attributes_['icons'];
 };
 
+
 /**
  * @return {boolean} True if the ad is a linear, false for non linear.
  */
@@ -436,13 +448,9 @@ VpaidAd.prototype.getAdLinear = function() {
  * @param {string} message
  */
 VpaidAd.prototype.log = function(message) {
-  var logTextArea = document.getElementById('logTextArea');
-  if (typeof logTextArea != 'undefined' && logTextArea != null) {
-    var content = logTextArea.innerHTML;
-    content = message + '\n' + content;
-    logTextArea.innerHTML = content;
-  } else {
-    console.log(message);
+  var logTextArea = document.getElementById('lastVpaidEvent');
+  if (logTextArea != null) {
+    logTextArea.value = message;
   }
 };
 
@@ -463,7 +471,7 @@ VpaidAd.prototype.adClickThruHandler_ = function() {
       document.getElementById('clickThruPlayerHandels').value;
   this.log('AdClickThu(' + clickThruUrl + ',' +
       clickThruId + ',' + clickThruPlayerHandles + ')');
-  this.events_['AdClickThru'](
+  this.eventCallbacks_['AdClickThru'](
       clickThruUrl,
       clickThruId,
       clickThruPlayerHandles);
@@ -482,7 +490,7 @@ VpaidAd.prototype.adErrorHandler_ = function() {
   }
   var adError = document.getElementById('adErrorMsg').value;
   this.log('adError(' + adError + ')');
-  this.events_['AdError'](adError);
+  this.eventCallbacks_['AdError'](adError);
 };
 
 
@@ -498,7 +506,7 @@ VpaidAd.prototype.adLogHandler_ = function() {
   }
   var adLogMsg = document.getElementById('adLogMsg').value;
   this.log('adLog(' + adLogMsg + ')');
-  this.events_['AdLog'](adLogMsg);
+  this.eventCallbacks_['AdLog'](adLogMsg);
 };
 
 
@@ -514,7 +522,7 @@ VpaidAd.prototype.adInteractionHandler_ = function() {
   }
   var adInteraction = document.getElementById('adInteractionId').value;
   this.log('adLog(' + adInteraction + ')');
-  this.events_['AdInteraction'](adInteraction);
+  this.eventCallbacks_['AdInteraction'](adInteraction);
 };
 
 
@@ -524,24 +532,36 @@ VpaidAd.prototype.adInteractionHandler_ = function() {
  * @private
  */
 VpaidAd.prototype.eventSelected_ = function() {
+  var clickThruParams = document.getElementById('AdClickThruOptions');
+  var adErrorParams = document.getElementById('AdErrorOptions');
+  var adLogParams = document.getElementById('AdLogOptions');
+  var adInteractionParams = document.getElementById('AdInteractionOptions');
+  clickThruParams.style.display = 'none';
+  adErrorParams.style.display = 'none';
+  adLogParams.style.display = 'none';
+  adInteractionParams.style.display = 'none';
+
   var eventSelect = document.getElementById('eventSelect');
   var value = eventSelect.value;
-  this.log(value);
-  if (value in this.events_) {
-    if (this.events_[value] != null) {
-      this.events_[value](); // Call subscribed listener.
-    }
+  if (value == 'AdClickThru') {
+    clickThruParams.style.display = 'inline';
+  } else if (value == 'AdError') {
+    adErrorParams.style.display = 'inline';
+  } else if (value == 'AdLog') {
+    adLogParams.style.display = 'inline';
+  } else if (value == 'AdInteraction') {
+    adInteractionParams.style.display = 'inline';
   }
 };
 
 
 /**
  * @param {string} eventName
- * @return {Boolean} True if this.events_ contains the callback.
+ * @return {Boolean} True if this.eventCallbacks_ contains the callback.
  * @private
  */
 VpaidAd.prototype.isEventSubscribed_ = function(eventName) {
-  return typeof(this.events_[eventName]) === 'function';
+  return typeof(this.eventCallbacks_[eventName]) === 'function';
 };
 
 
@@ -552,8 +572,8 @@ VpaidAd.prototype.isEventSubscribed_ = function(eventName) {
  */
 VpaidAd.prototype.fillProperties_ = function() {
   for (var key in this.attributes_) {
-    var textBox = document.getElementById(key);
-    textBox.setAttribute('value', this.attributes_[key]);
+    var span = document.getElementById(key);
+    span.textContent = this.attributes_[key];
   }
 };
 
@@ -566,4 +586,3 @@ VpaidAd.prototype.fillProperties_ = function() {
 var getVPAIDAd = function() {
   return new VpaidAd();
 };
-
