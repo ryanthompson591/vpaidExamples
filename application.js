68e4393e0984800370a5e7952ed8309d68ec5974
Ryan Thompson <ryanthompson591@gmail.com>
2015-07-14 16:23:16 -0400

    fixed typo

--- application.js
+++ application.js
@@ -130,7 +130,7 @@ Application.prototype.setXml_ = function() {
 
 Application.prototype.onClick_ = function() {
   if (!this.adsDone_) {
-    this.log_('Click event.');
+    this.log('Click event.');
     if (this.xmlBox_.value == '') {
       this.log("Error: please fill in xml");
       return;
